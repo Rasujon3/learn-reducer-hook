4dcdc645d6f77bbb06de8c2d5fbef7a6318362ca
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-09-07 01:16:46 +0600

    payload
    End of module 44

--- src/components/PatientManagement/PatientManagement.js
+++ src/components/PatientManagement/PatientManagement.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useReducer } from 'react/cjs/react.development';
 import { patientReducer, patientState } from '../reducer/patientReducer';
 
 const PatientManagement = () => {
+    const nameRef = useRef();
     const [state,dispatch] = useReducer(patientReducer,patientState);
-    console.log(state);
+    const handleSubmit = event => {
+        event.preventDefault();
+        dispatch({
+            type:'ADD_PATIENT',
+            name:nameRef.current.value,
+            id:state.patients.length + 1
+        })
+        nameRef.current.value = '';
+    }
     return (
         <div>
             <h1>Patient ManageMent: {state.patients.length}</h1>
-            <form action="">
-                <input type="text" ></input>
+            <form onSubmit={handleSubmit}>
+                <input ref={nameRef} ></input>
             </form>
+            {
+                state.patients.map(pt=> <li 
+                    key={pt.id}
+                    onClick={()=>dispatch({type:'REMOVE_PATIENT',id: pt.id})}
+                    >{pt.name}</li> )
+            }
         </div>
     );
 };
